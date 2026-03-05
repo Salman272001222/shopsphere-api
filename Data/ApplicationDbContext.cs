dacfe6e19c7591e515f0373c59a3d407ce5fad54
Salman272001222 <sa27sasa27@email.com>
2026-03-05 17:59:28 +0530

    Day 1 - Initial project setup with EF Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSphere.API.Models;
+
+namespace ShopSphere.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
